0bbfde7ba00d9a0bdd5b9d6c8601bf4adeb038b0
ariful-tushar <arifultusharr@gmail.com>
2020-07-29 14:00:30 +0600

    styling the menu component

---
+++ src/component/menu-items/menu-items.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+// import Directory from "../directory/directory.component.jsx";
+
+import "./menu-items.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="backgroundImage"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    ></div>
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">Shop Now</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+import Directory from "../../component/directory/directory.component.jsx";
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Homepage;
